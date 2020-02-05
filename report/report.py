de1190f3e08569a5f260764c1b032c3d2e99a3a8
Mitchell <mitchellbhuysen@hotmail.com>
2020-02-05 12:18:45 +0100

    fixed spelling error, added a total at the end and fixed some math errors

--- report/report.py
+++ report/report.py
@@ -13,14 +13,15 @@ database_login_info = {
     "database":"profile",
     "host":socket.gethostbyname("profile_odin")
 }
+total_res = 0
 body = ""
 symbols = lib.get_symbols(**database_login_info)
 for symbol in symbols:
     total, percentage, current, _100ra = lib.get_total_percentage_current_value_100ra(symbol,**database_login_info)
-    body += f"""{str(symbol)}:\n\t100ra:\t\t\t\t{str(_100ra)} vs. {current}\n\tcurrent perf:\t{str(total)}\n\t\t\t\t\t\t\t\t{str(percentage)}%\n\n"""
+    body += f"""{str(symbol)}:\n\t100ra:\t\t\t\t{str(_100ra)} vs. current :{current}\n\tcurrent perf:\t{str(total)}\n\t\t\t\t\t\t\t\t{str(percentage)}%\n\n"""
+    total_res += total
 
-
-body += "\n\n\n\t\tThis report was generated automatically by Mitchell Berendhuysen"
+body += f"""\nTotal combined: {round(total_res,2)}\n\n\t\tThis report was generated automatically by Mitchell Berendhuysen"""
 
 
 with smtplib.SMTP(host="smtp.gmail.com",port=587) as smtp:
@@ -33,4 +34,4 @@ with smtplib.SMTP(host="smtp.gmail.com",port=587) as smtp:
     print(msg)
     smtp.sendmail(environ["email"], "mitchellbhuysen@hotmail.com",msg)
 
-print(f"""proces took {time()-c} seconds""")
+print(f"""process took {time()-c} seconds""")
