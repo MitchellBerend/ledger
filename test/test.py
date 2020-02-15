520e9e806333258d015e87123849cd56b4d0add0
Mitchell <mitchellbhuysen@hotmail.com>
2020-02-15 19:15:12 +0100

    added first test

--- test/test.py
+++ test/test.py
@@ -1,14 +1,50 @@
 import sys
-sys.path.append("/home/ledger/api_caller")
-sys.path.append("/home/ledger/landing_page")
-sys.path.append("/home/ledger/track_handler")
-sys.path.append("/home/ledger/wallet_interaction")
 import unittest
-
+sys.path.append("..")
+from api_caller import api_caller_lib
 
 class test(unittest.TestCase):
-    def test_(self):
-        pass
+    
+    def test_retrieve_data(self):
+        class mock_cur:
+            def __init__(self):
+                pass
+
+            def execute(self,a):
+                pass
+
+            def fetchall(self):
+                return [("1111-11-11",100,20,50,80,100000,"MSFT","random_hash") for _ in range(100)]
+
+        class mock_con:
+            def __init__(self,**_):
+                self.cur = mock_cur()
+            
+            def cursor(self):
+                return self.cur
+            
+            def close(self):
+                pass
+
+        api_caller_lib.sql.connect = mock_con
+        api_caller_lib.sql.cursor = mock_cur
+        self.assertEqual(api_caller_lib.retrieve_data("msft"),[("1111-11-11",100,20,50,80,100000,"MSFT","random_hash") for _ in range(100)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
