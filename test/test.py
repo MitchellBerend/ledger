a87b70f621dd26121d18241b4462220ead00b0f5
Mitchell <mitchellbhuysen@hotmail.com>
2020-02-15 15:10:34 +0100

    test commit

--- test/test.py
+++ test/test.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.append("/home/ledger/api_caller")
 sys.path.append("/home/ledger/landing_page")
-sys.path.append("/home/ledger/maintenance")
-sys.path.append("/home/ledger/report")
 sys.path.append("/home/ledger/track_handler")
 sys.path.append("/home/ledger/wallet_interaction")
 import unittest
@@ -11,3 +9,8 @@ import unittest
 class test(unittest.TestCase):
     def test_(self):
         pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
